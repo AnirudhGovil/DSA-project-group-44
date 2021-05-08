9156a805e4d3bed407c5dcc00f9c1885b24e5777
Sky_Walker <vamsikrishna2032002@gmail.com>
2021-05-08 18:33:33 +0530

    updated Create

--- Project/dsa/utils/createfolder/createfolder.c
+++ Project/dsa/utils/createfolder/createfolder.c
@@ -2,16 +2,21 @@
 
 int createfolder(char **args)
 {
-    const char cmd1[10001];
-    const char cmd2[10001];
+    char cmd1[10001];
+    char cmd2[10001];
     char s1[1024];
+    char d2[1024];
     char d1[1024];
+    getcwd(s1, 1023); //to get the current working directory
+
     strcpy(d1, "/home/vamsi/Downloads/dsa_assignment/");
-    getcwd(s1, 1023);
+    //sprintf(d2,"%s/Downloads/",s1);
 
     sprintf(cmd1, "mkdir %s/%s", s1, args[1]);
-    system(cmd1);
-    sprintf(cmd2, "cp -a %s. %s/%s", d1, s1, args[1]);
+    system(cmd1); //to create empty directory with name arg[1]
+
+    sprintf(cmd2, "cp -a %s. %s/%s", d1, s1, args[1]); //to copy files from Downloads to newly created directory
+    //sprintf(cmd2, "cp -a %s. %s/%s", d2, s1, args[1]);
     system(cmd2);
 
     return 1;

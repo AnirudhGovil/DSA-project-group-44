92951a2effa39b5b8d99e8d8777c287b46738148
Sky_Walker <vamsikrishna2032002@gmail.com>
2021-05-08 19:21:37 +0530

    Update and Create functions are modified

--- Project/dsa/utils/createfolder/createfolder.c
+++ Project/dsa/utils/createfolder/createfolder.c
@@ -4,20 +4,21 @@ int createfolder(char **args)
 {
     char cmd1[10001];
     char cmd2[10001];
-    char s1[1024];
-    char d2[1024];
-    char d1[1024];
-    getcwd(s1, 1023); //to get the current working directory
+    char current_directory[1024];
+    char download_directory2[1024];
+    char download_directory1[1024];
+    getcwd(current_directory, 1023); //to get the current working directory
 
-    strcpy(d1, "/home/vamsi/Downloads/dsa_assignment/");
-    //sprintf(d2,"%s/Downloads/",s1);
+    strcpy(download_directory1, "/home/vamsi/Downloads/dsa_assignment/");
+    //sprintf(download_directory2,"%s/Downloads/",current_directory);
 
-    sprintf(cmd1, "mkdir %s/%s", s1, args[1]);
+    sprintf(cmd1, "mkdir %s/%s", current_directory, args[1]);
     system(cmd1); //to create empty directory with name arg[1]
 
-    sprintf(cmd2, "cp -a %s. %s/%s", d1, s1, args[1]); //to copy files from Downloads to newly created directory
-    //sprintf(cmd2, "cp -a %s. %s/%s", d2, s1, args[1]);
+    sprintf(cmd2, "cp -a %s. %s/%s", download_directory1, current_directory, args[1]); //to copy files from Downloads to newly created directory
+    //sprintf(cmd2, "cp -a %s. %s/%s", download_directory2, current_directory, args[1]);
     system(cmd2);
+    printf("%s is created and downloaded the required files\n",args[1]);
 
     return 1;
 }
--- Project/dsa/utils/updatefolder/updatefolder.h
+++ Project/dsa/utils/updatefolder/updatefolder.h
@@ -1,4 +1,11 @@
 #ifndef UPDATEFOLDER_H
 #define UPDATEFOLDER_H
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
 int updatefolder(char **args);
 #endif

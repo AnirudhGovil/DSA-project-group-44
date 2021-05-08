011056bacad15f090ff00e10377be156127866c4
Sky_Walker <vamsikrishna2032002@gmail.com>
2021-05-08 18:17:52 +0530

    Added function Create

--- Project/dsa/utils/createfolder/createfolder.c
+++ Project/dsa/utils/createfolder/createfolder.c
@@ -2,11 +2,17 @@
 
 int createfolder(char **args)
 {
-    /* char cmd1[10001];
-    char cmd2[10001];
-    sprintf(cmd1,"mkdir %s",args[1]);
+    const char cmd1[10001];
+    const char cmd2[10001];
+    char s1[1024];
+    char d1[1024];
+    strcpy(d1, "/home/vamsi/Downloads/dsa_assignment/");
+    getcwd(s1, 1023);
+
+    sprintf(cmd1, "mkdir %s/%s", s1, args[1]);
     system(cmd1);
-    sprintf(cmd2,"cp -r %s .",args[1]);
+    sprintf(cmd2, "cp -a %s. %s/%s", d1, s1, args[1]);
     system(cmd2);
-    return 0; */
+
+    return 1;
 }
--- Project/dsa/utils/createfolder/createfolder.h
+++ Project/dsa/utils/createfolder/createfolder.h
@@ -1,6 +1,11 @@
 #ifndef CREATEFOLDER_H
 #define CREATEFOLDER_H
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
 int createfolder(char **args);
 #endif

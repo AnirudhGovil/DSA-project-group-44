612e3a66ed8118e26945b619a00f48188dbd55c9
Sky_Walker <vamsikrishna2032002@gmail.com>
2021-05-08 22:14:54 +0530

    Update updatefolder.c

--- Project/dsa/utils/updatefolder/updatefolder.c
+++ Project/dsa/utils/updatefolder/updatefolder.c
@@ -12,7 +12,7 @@ int updatefolder(char **args)
     //sprintf(download_directory2,"%s/Downloads/",current_directory);
 
     sprintf(cmd1, "rm -r %s/%s/dist", current_directory, args[1]);
-    system(cmd1); //to delete old assignment files in directory arg[1]
+    system(cmd1); //to delete old dist files in directory arg[1]
 
     sprintf(cmd2, "cp -a %s. %s/%s", download_directory1, current_directory, args[1]); //to copy files from Downloads to newly created directory
     //sprintf(cmd2, "cp -a %s. %s/%s", download_directory2, current_directory, args[1]);

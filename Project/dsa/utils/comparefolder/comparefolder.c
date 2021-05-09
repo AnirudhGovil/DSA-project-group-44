d758e7a28857662c29761bef784eb5ebcf36cb3d
vaibhv22 <83178116+vaibhv22@users.noreply.github.com>
2021-05-09 19:16:18 +0530

    Update comparefolder.c

--- Project/dsa/utils/comparefolder/comparefolder.c
+++ Project/dsa/utils/comparefolder/comparefolder.c
@@ -1,6 +1,34 @@
 #include "comparefolder.h"
+
 int comparefolder(char **args)
 {
+    // args[1] -> assignment 
+    // args[2] -> zipfile
+    char cmd1[10001];
+    char cmd2[10001];
+    char cmd3[10001];
+    char current_directory[1024];
+    char download_directory[1024];
+    getcwd(current_directory, 1023);
+    // download_directory = " ";
+    char * z = "/";
+    FILE * fileptr;
+    strcat(current_directory, z);
+    strcat(current_directory,args[1]);
+    fileptr = fopen(current_directory , "r");
+    strcat(download_directory, z);
+    strcat(download_directory,args[2]);
+    if(fileptr == NULL){
+        printf("NO SUCH FILE TO COMPARE\n");
+    }
+    else{
+    sprintf(cmd1,"find /%s/ -type f -exec md5sum {} + | sort -k 2 > dir1.txt",current_directory);
+    system(cmd1);
+    sprintf(cmd2,"find /%s/ -type f -exec md5sum {} + | sort -k 2 > dir2.txt",download_directory);
+    system(cmd2);
+    sprintf(cmd3,"diff -r <dir1.txt> <dir2.txt>");
+    system(cmd3);
+    }
     return 0;
 }
 

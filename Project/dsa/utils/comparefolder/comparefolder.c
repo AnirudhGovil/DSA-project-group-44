054be106e4d988093ba7f7b1ab05a6ca7858c630
AnirudhGovil <anirudh.govil@students.iiit.ac.in>
2021-05-10 23:31:23 +0400

    comparefolder and submitfolder now functional

--- Project/dsa/utils/comparefolder/comparefolder.c
+++ Project/dsa/utils/comparefolder/comparefolder.c
@@ -77,44 +77,34 @@ int comparefolder(char **args)
 
 /* #include "comparefolder.h"
 
-int exists(const char *fname)
-{
-    FILE *file;
-    if ((file = fopen(fname, "r")))
-    {
-        fclose(file);
-        return 1;
-    }
-    return 0;
-}
-
 int comparefolder(char **args)
 {
-    // args[1] -> assignment
+    // args[1] -> assignment 
     // args[2] -> zipfile
-    char home_d[100001];
-    char cmd1[100001];
-    char cmd2[200002];
-    char cmd3[300003];
-    getcwd(home_d, 100001);
+    char cmd1[20001];
+    char cmd2[20001];
+    char cmd3[10001];
+    char home_d[1024];
+    char dir1[10001];
+    char dir2[10001];
+    char download_directory[1024];
+    getcwd(home_d, 1024);
+    strcat(download_directory, "Downloads/");
+    strcat(download_directory,args[2]);
     if (chdir(args[1]) == -1)
     {
-        printf("No such Folder\n");
+        printf("NO SUCH FILE TO COMPARE\n");
         return 1;
     }
     chdir(home_d);
-    if (!exists(args[2]))
-    {
-        printf("No such Folder\n");
-        return 1;
-    }
 
-    sprintf(cmd1,"diff <(find %s -type f -exec md5sum {} + | sort -k 2) <(find %s -type f -exec md5sum {} + | sort -k 2)",args[1],args[2]);
+    sprintf(cmd1,"find %s -type f -exec md5sum {} + | sort -k 2 > dir1.txt",args[1]);
     system(cmd1);
-    sprintf(cmd2, "find %s -type f -exec md5sum {} + | sort -k 2 > dir2.txt", args[2]);
+    sprintf(cmd2,"find %s -type f -exec md5sum {} + | sort -k 2 > dir2.txt",dir2);
     system(cmd2);
-    sprintf(cmd3, "diff -r <dir1.txt> <dir2.txt>");
+    sprintf(cmd3,"diff -r <dir1.txt> <dir2.txt>");
     system(cmd3);
- 
+    
     return 0;
-} */
+}
+ */
